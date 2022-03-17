66d5d8535049a307793749714c7fc89202a28b73
🔥🐻 <firebear.dev@gmail.com>
2022-03-17 10:41:26 +0100

    Adding AttackStatus enum on Attack component with Done method

--- Assets/Scripts/Components/Attack.cs
+++ Assets/Scripts/Components/Attack.cs
@@ -2,12 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackStatus
+{
+    still, 
+    
+    trying,
+
+    hit
+}
+
 [RequireComponent(typeof(SelectedCharacter))]
 public class Attack : MonoBehaviour
 {
     [SerializeField] private SelectedCharacter _selected;
 
-    public float strenght => _selected.actualProperties.strenght;
+    public AttackStatus status { get; private set; }
 
-    private void Reset() => _selected ??= GetComponent<SelectedCharacter>();
+    public float damage => _selected.actualProperties.strenght;
+
+    private void Reset()
+    {
+        _selected ??= GetComponent<SelectedCharacter>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        status = AttackStatus.still;
+    }
+
+    public void Done()
+    {
+        status = AttackStatus.hit;
+    }
 }
