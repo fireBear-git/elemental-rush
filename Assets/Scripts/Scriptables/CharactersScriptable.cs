9a3e3b6f55d5ca0c5e9619dd4cfa5d08792dc518
🔥🐻 <firebear.dev@gmail.com>
2022-03-17 09:51:31 +0100

    Add GetCharacter Method for a better cohesion of CharacterScriptable.cs

--- Assets/Scripts/Scriptables/CharactersScriptable.cs
+++ Assets/Scripts/Scriptables/CharactersScriptable.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 public class CharactersScriptable : ScriptableObject
 {
     [SerializeField] private Character[] _characters;
+
+    public Character GetCharacter(string id)
+    {
+        for (int i = 0; i < _characters.Length; i++)
+        {
+            if(_characters[i].id == id)
+                return _characters[i];
+        }
+
+        return null;
+    }
 }
