4d2d16307929ffca047fea427dcd38a1081a3e78
🔥🐻 <firebear.dev@gmail.com>
2022-03-19 10:56:06 +0100

    Motion component: Removed requirement from CharacterController

--- Assets/Scripts/Components/Motion.cs
+++ Assets/Scripts/Components/Motion.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SelectedCharacter))]
-[RequireComponent(typeof(CharacterController))]
 public class Motion : MonoBehaviour
 {
     [Header("Fields")]
