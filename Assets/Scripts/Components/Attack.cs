4f47074290b2a6915f18b2a047230e6d9c3ca3d1
🔥🐻 <firebear.dev@gmail.com>
2022-03-21 16:52:21 +0100

    Attack component: adding fury-related fields for improve mechanic of special attacks

--- Assets/Scripts/Components/Attack.cs
+++ Assets/Scripts/Components/Attack.cs
@@ -8,6 +8,8 @@ public enum AttackStatus
     
     trying,
 
+    tryingSpecial,
+
     hit
 }
 
@@ -15,21 +17,40 @@ public enum AttackStatus
 [RequireComponent(typeof(SelectedCharacter))]
 public class Attack : MonoBehaviour
 {
+    [Header("Fields")]
+    [SerializeField] private float _maxFury = 4f;
+    
+    [Header("Components")]
     [SerializeField] private SelectedCharacter _selected;
+
+    [Header("Scriptables Actions")]
+    [SerializeField] private ScriptableActionFloat _specialAttackAction;
+
+    private float _actualFury;
 
     public AttackStatus status { get; private set; }
 
     public float damage => _selected.actualProperties.strenght;
 
+    #region Unity Callbacks
+    
     private void Reset()
     {
         _selected ??= GetComponent<SelectedCharacter>();
+    }
+
+    private void Start()
+    {
+        _actualFury = 0f;
+        _specialAttackAction?.Invoke(_actualFury / _maxFury);
     }
 
     private void OnTriggerExit(Collider other)
     {
         status = AttackStatus.still;
     }
+
+    #endregion
 
     public void Hit()
     {
@@ -39,13 +60,21 @@ public class Attack : MonoBehaviour
 
     public void SpecialHit()
     {
-
-        status = AttackStatus.trying;
-        _selected.SetTrigger($"special_{AttackStatus.trying}");
+        if(_actualFury > 1f)
+        {
+            _actualFury--;
+            status = AttackStatus.tryingSpecial;
+            _selected.SetTrigger($"special_{AttackStatus.trying}");
+            _specialAttackAction?.Invoke(_actualFury / _maxFury);
+        }
     }
-
+    
     public void Done()
     {
+        if(status != AttackStatus.tryingSpecial)
+            _actualFury += 0.2f;
+        
         status = AttackStatus.hit;
+        _specialAttackAction?.Invoke(_actualFury / _maxFury);
     }
 }
